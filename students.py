f62013e5ae869aed00dd1ac0eba214d6bba086d3
K-Senthil-Shunmugam <113205555+K-Senthil-Shunmugam@users.noreply.github.com>
2023-08-24 23:01:19 +0530

    Update students.py

--- students.py
+++ students.py
@@ -10,6 +10,9 @@ client = pymongo.MongoClient(st.secrets["CONNECTION_STRING"])
 db = client["Lumina"]
 collection = db["student"]
 
+# Initialize conversation
+conversation = []
+
 # Streamlit app
 def main():
     # Fetch domains from MongoDB
@@ -37,10 +40,9 @@ def main():
             if submitted:
                 # Store user selections in conversation
               
-                conversation = [
-                {"role": "system", "content": f"You are a leader in {selected_subcategory} providing recommendations to a new individual who is a aspirant for the {selected_subcategory}."},
-                {"role": "user", "content": f"I am currently interested in learning advanced skillsets in {selected_job_role} in {selected_subcategory} under the domain of {selected_domain}. Please provide recommendations on advance skillset , technical toolkits : list the skillsets and toolkits only in the form of a table ."}
-                ]
+                
+                conversation.append({"role": "system", "content": f"You are a leader in {selected_subcategory} providing recommendations to a new individual who is a aspirant for the {selected_subcategory}."})
+                conversation.append({"role": "user", "content": f"I am currently interested in learning advanced skillsets in {selected_job_role} in {selected_subcategory} under the domain of {selected_domain}. Please provide recommendations on advance skillset , technical toolkits : list the skillsets and toolkits only in the form of a table ."})
 
                 # Make an API call to OpenAI for recommendations
                 response = openai.ChatCompletion.create(
@@ -53,6 +55,26 @@ def main():
                 ai_response = response['choices'][0]['message']['content']
                 st.subheader("Lumina's Recommendations:")
                 st.write(ai_response)
+
+    # Sidebar chat input
+    st.sidebar.subheader("Chat with Lumina:")
+    user_input = st.sidebar.text_input("You:", "")
+
+    if user_input:
+        # Add user input to the conversation
+        conversation.append({"role": "user", "content": user_input})
+
+        # Continue the conversation with OpenAI
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=conversation,
+            max_tokens=1000  # Adjust the max tokens as needed
+        )
+
+        # Extract and display the AI's response
+        ai_response = response['choices'][0]['message']['content']
+        st.subheader("Lumina's Response:")
+        st.write("Lumina: " + ai_response)
 
 if __name__ == "__main__":
     main()
